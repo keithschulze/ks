0033d597ff5a67495be9d4c64ef34101bdcffdbf
Keith Schulze <keith.schulze@hey.com>
2022-11-10 21:13:24 +1300

    Add notebook module for storing blog related notebook
    - Update Rust functional image notebook to be compatible with recent
      evcxr versions

---
+++ notebooks/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A simple flake for a projects";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    rust-overlay.url = github:oxalica/rust-overlay;
+
+    flake-utils = {
+      url = github:numtide/flake-utils;
+    };
+
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          name = "ks-jupyter";
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+
+          rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" ];
+            targets = [ "wasm32-unknown-unknown" ];
+          };
+        in
+          {
+            devShells.default = pkgs.mkShell rec {
+              buildInputs = with pkgs; [
+                bashInteractive
+                openssl
+                pkg-config
+                exa
+                fd
+                evcxr
+                rust
+                python310
+                poetry
+                darwin.apple_sdk.frameworks.IOKit
+              ];
+
+              shellHook = ''
+                set +e
+                if [[ ! -d ./.venv ]]; then
+                  python -m venv .venv
+                fi
+
+                [[ -f poerty.lock ]] && poetry install
+              '';
+            };
+          }
+        );
+}
