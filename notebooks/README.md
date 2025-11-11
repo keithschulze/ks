efb0e609e890730af751860ea1cd500113c28e40
Keith Schulze <keith@schulze.co.nz>
2025-11-11 22:01:27 +1300

    Update broken notebook and switch to uv

---
+++ notebooks/README.md
@@ -0,0 +1,17 @@
+# Notebooks
+
+This subdirectory include Jupyter notebooks for my blog.
+
+## How to run
+
+The easiest is to have `nix` installed:
+
+```sh
+nix develop -c uv run jupyter lab
+```
+
+Alternatively, you can install [`uv`](https://docs.astral.sh/uv/getting-started/installation/). Then run:
+
+```sh
+uv run jupyter lab
+```
--- notebooks/flake.nix
+++ notebooks/flake.nix
@@ -1,53 +1,37 @@
 {
-  description = "A simple flake for a projects";
-
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    rust-overlay.url = github:oxalica/rust-overlay;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
 
     flake-utils = {
-      url = github:numtide/flake-utils;
+      url = "github:numtide/flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           overlays = [ (import rust-overlay) ];
-          name = "ks-jupyter";
           pkgs = import nixpkgs {
             inherit system overlays;
           };
 
           rust = pkgs.rust-bin.stable.latest.default.override {
             extensions = [ "rust-src" ];
-            targets = [ "wasm32-unknown-unknown" ];
           };
         in
           {
-            devShells.default = pkgs.mkShell rec {
+            devShells.default = pkgs.mkShell {
               buildInputs = with pkgs; [
                 bashInteractive
                 openssl
                 pkg-config
-                exa
-                fd
                 evcxr
                 rust
-                python310
-                poetry
-                darwin.apple_sdk.frameworks.IOKit
+                python313
+                uv
               ];
-
-              shellHook = ''
-                set +e
-                if [[ ! -d ./.venv ]]; then
-                  python -m venv .venv
-                fi
-
-                [[ -f poerty.lock ]] && poetry install
-              '';
             };
           }
         );
