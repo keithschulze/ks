3752d816f24588357dd806b2707044f569503709
Keith Schulze <keith@schulze.co.nz>
2025-11-18 20:40:51 +1300

    Pin tofu provider deps for dependabot

--- deploy/tf/proivder.tf
+++ deploy/tf/proivder.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "6.20.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.1"
     }
   }
 
